10a3dbae45d43aed2d3f41e9df779bda170d8393
jzha2102 <jzha2102@uni.sydney.edu.au>
2020-10-27 11:24:26 +1100

    dashboardData would return JSON

--- BusinessMap/src/main/java/com/business/system/controller/dashboardData.java
+++ BusinessMap/src/main/java/com/business/system/controller/dashboardData.java
@@ -14,6 +14,10 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
 import static com.mongodb.client.model.Filters.regex;
 
 
@@ -35,6 +39,7 @@ public class dashboardData {
         }
         MongoCollection<Document> collection = mongoTemplate.getCollection(name);
 
+
         if(address == null || address == ""){
             address = "2000"; // setDefault result
         }
@@ -55,19 +60,44 @@ public class dashboardData {
 
         //to json (string)
         StringBuilder sb = new StringBuilder();
-        sb.append("[");
+//        sb.append("[");
         for(Document document : documents) {
             sb.append(document.toJson() + ",");
         }
-        sb.append("]");
+//        sb.append("]");
 
-//        JSONObject jsonObj = new JSONObject(sb.toString());
+        JSONObject jsonObj = new JSONObject(sb.toString());
 //        int count = jsonObj.size();
 //        jsonObj.put("status", "ok");
 //        jsonObj.put("length", sb.length());
 //        jsonObj.put("devices", noPlateNumberCarForm.getDevicesId());
 
-      return sb.toString();
+
+
+//
+
+      return jsonObj.toString();
+    }
+
+
+    //        db.getCollectionNames().map(function(name) {
+//            return { "name": name, "count": db[name].count() }
+//        })
+
+
+
+    @RequestMapping("/getCollect")
+    public Set<String> getCollectionsList(String dbName) {
+
+        Set<String> resultList = mongoTemplate.getCollectionNames();
+
+        mongoTemplate.getCollection("industryCount");
+
+//        regex("results.formatted_address",address);
+
+
+
+        return resultList;
     }
 
 }
